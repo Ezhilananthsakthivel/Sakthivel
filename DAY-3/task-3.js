b91c62886a845275508f14ddf97837ca93f981e6
Ezhilananthsakthivel <71553660+Ezhilananthsakthivel@users.noreply.github.com>
2022-03-29 00:44:57 +0530

    Add files via upload

---
+++ DAY-3/task-3.js
@@ -0,0 +1,38 @@
+//for
+// const studentDetails = `[
+//   {"name":"Sakthivel","gender":"Male","phone no":"9087345799"},
+//   {"name":"Jagathish","gender":"Male","phone no":"9080454718"},
+//   {"name":"Karthick","gender":"Male","phone no":"7845858588"}
+// ]`
+
+// const json_sd = JSON.parse(studentDetails);
+// for(var i=0;i<json_sd.length;i++)
+// console.log(json_sd[i]);
+
+//For in
+// const studentDetails = '{"name":"Sakthivel","gender":"Male","phone no":"9087345799"}'
+// const json_sd = JSON.parse(studentDetails);
+// for(var i in json_sd)
+// console.log(i);
+
+//for of
+// const studentDetails =  `[
+//      {"name":"Sakthivel","gender":"Male","phone no":"9087345799"},
+//      {"name":"Jagathish","gender":"Male","phone no":"9080454718"},
+//      {"name":"Karthick","gender":"Male","phone no":"7845858588"}
+//    ]`
+// const json_sd = JSON.parse(studentDetails);
+// for(var i of json_sd)
+// console.log(i);
+
+
+// forEach
+// const studentDetails =  `[
+//      {"name":"Sakthivel","gender":"Male","phone no":"9087345799"},
+//      {"name":"Jagathish","gender":"Male","phone no":"9080454718"},
+//      {"name":"Karthick","gender":"Male","phone no":"7845858588"}
+//    ]`
+// const json_sd = JSON.parse(studentDetails);
+// json_sd.forEach(function(value,index){
+//   console.log(index+" "+JSON.stringify(value))
+// });
