75f30f495c08c6b92a1a00a0c9bdc309f1f2a7b6
Ezhilananthsakthivel <71553660+Ezhilananthsakthivel@users.noreply.github.com>
2022-04-25 10:16:07 +0530

    Update 1.js

--- DAY-4/1.js
+++ DAY-4/1.js
@@ -1,6 +1,13 @@
 //Compare two JSON have the same properties without order
-//Install the lodash library using npm
+
 var obj1 = { name: "Person 1", age:5 };
 var obj2 = { age:5, name: "Person 1" };
-var _= require('lodash');
-console.log(_.isEqual(obj1,obj2));
+
+var isEqual = (obj1,obj2)=>{
+    keys1 = Object.keys(obj1);
+    keys2 = Object.keys(obj2);
+
+    //return true when the two json has same length and all the properties has same value key by key
+    return keys1.length === keys2.length && Object.keys(obj1).every(key=>obj1[key]==obj2[key]);
+}
+console.log("json is equals: "+ isEqual(obj1,obj2));
